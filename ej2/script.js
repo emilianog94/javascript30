5fff2238cd80c34e6a6fafb32106dcef200259e0
Emiliano Gioia <emiliano@gioia.com.ar>
2021-05-25 15:51:07 -0300

    Ejercicio 2 - Reloj JS y CSS listo

---
+++ ej2/script.js
@@ -0,0 +1,23 @@
+const agujaSegundos = document.querySelector('.aguja.segundos');
+const agujaMinutos = document.querySelector('.aguja.minutos');
+const agujaHoras = document.querySelector('.aguja.horas');
+
+function changeClock(){
+    let currentDate = new Date();
+    let dateSeconds = currentDate.getSeconds();
+    let dateMinutes = currentDate.getMinutes();
+    let dateHours = currentDate.getHours();
+
+    // Debo traducir los segundos a grados
+    let gradesSeconds = (dateSeconds / 60) * 360;
+    agujaSegundos.style.transform = `rotate(${gradesSeconds}deg)`;
+
+    let gradesMinutes = (dateMinutes / 60) * 360;
+    agujaMinutos.style.transform = `rotate(${gradesMinutes}deg)`;
+
+    let gradesHours = (dateHours / 60) * 360;
+    agujaHoras.style.transform = `rotate(${gradesHours}deg)`;
+}
+
+changeClock();
+setInterval(changeClock,1000);
